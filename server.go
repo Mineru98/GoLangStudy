72a8e6b55d51a649a846d7674dc94de4978ec348
Mineru98 <rmstjr1030@nate.com>
2019-11-27 14:56:52 +0000

    Add Document

--- server.go
+++ server.go
@@ -1,3 +1,6 @@
+// http://golang.site/go/article/107-MySql-%EC%82%AC%EC%9A%A9---%EC%BF%BC%EB%A6%AC
+
+// https://blog.youngbin.xyz/2019-09-09-migrating-skhus-backend-from-nodejs-to-golang/
 package main
 
 import (
