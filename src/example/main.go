e44e36cf5b51263b161bbbb64ea5d36638b26650
Mineru98 <rmstjr1030@naver.com>
2021-01-20 22:39:36 +0900

    Update Code

--- src/example/main.go
+++ src/example/main.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-type Res struct {
-	Data	string	`json:"data"`
-}
-
 func main() {
-	slice1 := []int{1,2,3}
-    slice2 := append([]int{}, 4, 5)
-	fmt.Println(slice1, slice2)
-	
-	x := make(map[string]int)
-	x["key1"] = 10
-	x["key2"] = 20
-	fmt.Println(x)
-	fmt.Println(x["key1"])
-	fmt.Println(x["key2"])
-
-	bolJson, _ := json.Marshal(true)
-	fmt.Println(string(bolJson))
-
-	res := &Res{
-		Data: "test",
-	}
-	resJson, _ := json.Marshal(res)
-	fmt.Println(string(resJson))
+	var a int = 1
+	fmt.Println("hello world", a)
 }
