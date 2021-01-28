161ff0931d6af95b0c981d0a6f02c007888fb889
Mineru98 <rmstjr1030@naver.com>
2021-01-28 12:29:25 +0900

    Update Code

--- src/api/main.go
+++ src/api/main.go
@@ -3,6 +3,8 @@ package main
 import (
   "net/http"
   "github.com/gin-gonic/gin"
+  "api/routers/v1"
+  "api/middlewares/cors"
 )
 
 var db = make(map[string]string)
@@ -11,18 +13,19 @@ var router *gin.Engine
 func init() {
   // mysql.Sync()
   router = gin.New()
+  router.Use(cors.CorsMiddleware)
   app := router.Group("/v1")
   v1.InitRoutes(app)
 }
 
 func noRouteHandler() gin.HandlerFunc{
   return func(c *gin.Context){
-    var statusCode  int
-    var message     string
-    var data        interface{} = nil
-    // var listError   []models.ErroNodel = nil
-    var endPoint    string = c.Request.URL.String()
-    var method      string = c.Request.Method
+    // var statusCode  int
+    // var message     string
+    // var data        interface{} = nil
+    // // var listError   []models.ErroNodel = nil
+    // var endPoint    string = c.Request.URL.String()
+    // var method      string = c.Request.Method
   }
 }
 
